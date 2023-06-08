dcea5769fc4b076b52f7923b1a0be9ee7ecbff66
gina anifah <gina.anifah.tif20@polban.ac.id>
2023-06-09 00:04:58 +0700

    Mengubah tampilan halaman /404 dan /500

--- src/views/pages/page404/Page404.js
+++ src/views/pages/page404/Page404.js
@@ -7,14 +7,17 @@ import {
 import { Button, Result } from 'antd'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import '../../monitoring/pengisianDokumen/rpp/rpp.css'
+import { useLayoutEffect } from 'react'
 
 const Page404 = () => {
   const history = useHistory()
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "white"
+});
   return (
     <div className='container2'>
       <Result
-    status="400"
-    title="400"
+    title="404"
     subTitle="Sorry, Something Wrong."
     extra={<Button type="primary" onClick={()=>history.push(`/dashboard`)}>Back Home</Button>}
   />
--- src/views/pages/page500/Page500.js
+++ src/views/pages/page500/Page500.js
@@ -3,13 +3,16 @@ import { CCol, CContainer, CRow } from '@coreui/react'
 import { Button, Result } from 'antd'
 import '../../monitoring/pengisianDokumen/rpp/rpp.css'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { useLayoutEffect } from 'react'
 
 const Page500 = () => {
   const history = useHistory()
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "white"
+});
   return (
     <div className="container2">
       <Result
-        status="500"
         title="500"
         subTitle="Sorry, something went wrong."
         extra={
