889742286689a480c707ef0e655368993fe483f1
gina anifah <gina.anifah.tif20@polban.ac.id>
2023-06-08 18:56:19 +0700

    DETAIL RPP SELESAI *BE FE

--- src/views/monitoring/pengisianDokumen/rpp/detailRPP.js
+++ src/views/monitoring/pengisianDokumen/rpp/detailRPP.js
@@ -52,105 +52,126 @@ const DetailRPP = (props) => {
         .then((response) => {
           console.log(response.data.data)
 
-        //   setDataRPP({
-        //     tanggal_mulai: response.data.data[0].attributes.tanggal_mulai,
-        //     tanggal_selesai: response.data.data[0].attributes.tanggal_selesai,
-        //     perankelompok: response.data.data[0].attributes.perankelompok,
-        //     waktupengisian: response.data.data[0].attributes.waktupengisian,
-        //     judulpekerjaan: response.data.data[0].attributes.judulpekerjaan,
-        //     deskripsi_tugas: response.data.data[0].attributes.deskripsi_tugas,
-        //     status: response.data.data[0].status,
-        //   })
+          const convertDate = (date) => {
+            var temp_date_split = date.split('-')
+            const month = [
+              'Januari',
+              'Februari',
+              'Maret',
+              'April',
+              'Mei',
+              'Juni',
+              'Juli',
+              'Agustus',
+              'September',
+              'Oktober',
+              'November',
+              'Desember',
+            ]
+            var date_month = temp_date_split[1]
+            var month_of_date = month[parseInt(date_month) - 1]
+            return date ? `${temp_date_split[2]}  ${month_of_date}  ${temp_date_split[0]}` : null
+          }
 
-        //   /** SET DATA MILESTONES */
-        //   let temp_mil = []
-        //   let temp_mil1 = []
-        //   temp_mil = response.data.data[0].attributes.milestones.data
-        //   let temp_milestone = function (obj) {
-        //     for (var i in obj) {
-        //       temp_mil1.push({
-        //         id: obj[i].id,
-        //         deskripsi: obj[i].attributes.deskripsi,
-        //         tanggalmulai: obj[i].attributes.tanggalmulai,
-        //         tanggalselesai: obj[i].attributes.tanggalselesai,
-        //       })
-        //     }
-        //   }
+          setDataRPP({
+            start_date: convertDate(response.data.data.start_date),
+            finish_date: convertDate(response.data.data.finish_date),
+            group_role: response.data.data.group_role,
+            work_title: response.data.data.work_title,
+            task_description: response.data.data.task_description,
+          })
 
-        //   temp_milestone(temp_mil)
-        //   setDataMilestones(temp_mil1)
-        //   console.log('milestones', temp_mil1)
+              /**SET DATA DELIVERABLES */
+              let temp_del = []
+              let temp_del1 = []
+              temp_del = response.data.data.deliverables
+              let temp_deliverables = function (obj) {
+                for (var i in obj) {
+                  temp_del1.push({
+                    id: obj[i].id,
+                    due_date: convertDate(obj[i].due_date),
+                    deliverables: obj[i].deliverables,
+                  })
+                }
+              }
+    
+              temp_deliverables(temp_del)
+              setDataDeliverables(temp_del1)
+              console.log('deliverables', temp_del1)
 
-        //   /**SET DATA DELIVERABLES */
-        //   let temp_del = []
-        //   let temp_del1 = []
-        //   temp_del = response.data.data[0].attributes.deliverables.data
-        //   let temp_deliverables = function (obj) {
-        //     for (var i in obj) {
-        //       temp_del1.push({
-        //         id: obj[i].id,
-        //         duedate: obj[i].attributes.duedate,
-        //         deliverables: obj[i].attributes.deliverables,
-        //       })
-        //     }
-        //   }
+          /** SET DATA MILESTONES */
+          let temp_mil = []
+          let temp_mil1 = []
+          temp_mil = response.data.data.milestones
+          let temp_milestone = function (obj) {
+            for (var i in obj) {
+              temp_mil1.push({
+                id: obj[i].id,
+                description: obj[i].description,
+                start_date: convertDate(obj[i].start_date),
+                finish_date: convertDate(obj[i].finish_date),
+              })
+            }
+          }
 
-        //   temp_deliverables(temp_del)
-        //   setDataDeliverables(temp_del1)
-        //   console.log('deliverables', temp_del1)
+          temp_milestone(temp_mil)
+          setDataMilestones(temp_mil1)
+          console.log('milestones', temp_mil1)
 
-        //   /**SET DATA RENCANA CAPAIAN MINGGUAN */
-        //   let temp_rcm = []
-        //   let temp_rcm1 = []
-        //   temp_rcm = response.data.data[0].attributes.rencanacapaianmingguans.data
-        //   let temp_rencanaCapaianMingguan = function (obj) {
-        //     for (var i in obj) {
-        //       temp_rcm1.push({
-        //         id: obj[i].id,
-        //         rencanacapaian: obj[i].attributes.rencanacapaian,
-        //         tanggalmulai: obj[i].attributes.tanggalmulai,
-        //         tanggalberakhir: obj[i].attributes.tanggalberakhir,
-        //       })
-        //     }
-        //   }
+      
 
-        //   temp_rencanaCapaianMingguan(temp_rcm)
-        //   setDataCapaianMingguan(temp_rcm1)
-        //   console.log('capaian mingguan', temp_rcm1)
+          /**SET DATA RENCANA CAPAIAN MINGGUAN */
+          let temp_rcm = []
+          let temp_rcm1 = []
+          temp_rcm = response.data.data.weekly_achievement_plans
+          let temp_rencanaCapaianMingguan = function (obj) {
+            for (var i in obj) {
+              temp_rcm1.push({
+                id: obj[i].id,
+                achievement_plan: obj[i].achievement_plan,
+                start_date: convertDate(obj[i].start_date),
+                finish_date: convertDate(obj[i].finish_date),
+              })
+            }
+          }
 
-        //   /** JADWAL PENYELESAIAN KESELURUHAN */
-        //   let temp_jadwalKeseluruhan = []
-        //   let temp_jadwalKeseluruhan1 = []
-        //   temp_jadwalKeseluruhan = response.data.data[0].attributes.jadwalpenyelesaiankeseluruhans.data
-        //   let temp_jadwalKeseluruhans = function (obj) {
-        //     for (var i in obj) {
-        //       temp_jadwalKeseluruhan1.push({
-        //         id: obj[i].id,
-        //         jenispekerjaan: obj[i].attributes.jenispekerjaan,
-        //         butirpekerjaan: obj[i].attributes.butirpekerjaan,
-        //         tanggalmulai: obj[i].attributes.tanggalmulai,
-        //         tanggalselesai: obj[i].attributes.tanggalselesai,
-        //       })
-        //     }
-        //   }
+          temp_rencanaCapaianMingguan(temp_rcm)
+          setDataCapaianMingguan(temp_rcm1)
+          console.log('capaian mingguan', temp_rcm1)
 
-        //   temp_jadwalKeseluruhans(temp_jadwalKeseluruhan)
-        //   setDataJadwalPenyelesaianKeseluruhan(temp_jadwalKeseluruhan1)
-        //   console.log('capaian mingguan', temp_jadwalKeseluruhan1)
-        // })
-        // .catch(function (error) {
-        //   if (error.toJSON().status >= 300 && error.toJSON().status <= 399) {
-        //     history.push({
-        //       pathname: '/login',
-        //       state: {
-        //         session: true,
-        //       },
-        //     })
-        //   } else if (error.toJSON().status >= 400 && error.toJSON().status <= 499) {
-        //     history.push('/404')
-        //   } else if (error.toJSON().status >= 500 && error.toJSON().status <= 599) {
-        //     history.push('/500')
-        //   }
+          /** JADWAL PENYELESAIAN KESELURUHAN */
+          let temp_jadwalKeseluruhan = []
+          let temp_jadwalKeseluruhan1 = []
+          temp_jadwalKeseluruhan = response.data.data.completion_schedules
+          let temp_jadwalKeseluruhans = function (obj) {
+            for (var i in obj) {
+              temp_jadwalKeseluruhan1.push({
+                id: obj[i].id,
+                task_type: obj[i].task_type,
+                task_name: obj[i].task_name,
+                start_date: convertDate(obj[i].start_date),
+                finish_date: convertDate(obj[i].finish_date),
+              })
+            }
+          }
+
+          temp_jadwalKeseluruhans(temp_jadwalKeseluruhan)
+          setDataJadwalPenyelesaianKeseluruhan(temp_jadwalKeseluruhan1)
+          console.log('capaian mingguan', temp_jadwalKeseluruhan1)
+        })
+        .catch(function (error) {
+          if (error.toJSON().status >= 300 && error.toJSON().status <= 399) {
+            history.push({
+              pathname: '/login',
+              state: {
+                session: true,
+              },
+            })
+          } else if (error.toJSON().status >= 400 && error.toJSON().status <= 499) {
+            history.push('/404')
+          } else if (error.toJSON().status >= 500 && error.toJSON().status <= 599) {
+            history.push('/500')
+          }
         })
     }
 
@@ -164,7 +185,7 @@ const DetailRPP = (props) => {
 
   return (
     <>
-      {/* <div className="container2">
+      <div className="container2">
       <React.Fragment>
       <Space wrap className="title-s">
        
@@ -174,7 +195,7 @@ const DetailRPP = (props) => {
         <h1 className="justify">RENCANA PENYELESAIAN PROYEK</h1>
         <div className="spacebottom"></div>
         <Box sx={{ color: 'primary.main' }}>
-          Tanggal RPP : {dataRPP.tanggal_mulai} &nbsp;s/d&nbsp; {dataRPP.tanggal_selesai}
+          Tanggal RPP : {dataRPP.start_date} &nbsp;&nbsp;s/d&nbsp;&nbsp; {dataRPP.finish_date}
         </Box>
         <Box sx={{ color: 'primary.main' }}>Dikumpulkan Pada : 15 Januari 2023</Box>
         <div className="spacebottom"></div>
@@ -183,7 +204,7 @@ const DetailRPP = (props) => {
           <Table sx={{ minWidth: 650 }} aria-label="caption table">
             <b className="left">Topik/Tema/Judul Pekerjaan</b>
             <TableRow>
-              <TableCell colSpan={3}>{dataRPP.judulpekerjaan}</TableCell>
+              <TableCell colSpan={3}>{dataRPP.work_title}</TableCell>
               <TableCell />
               <TableCell />
             </TableRow>
@@ -195,7 +216,7 @@ const DetailRPP = (props) => {
           <Table sx={{ minWidth: 650 }} aria-label="caption table">
             <b className="left">Peran Kelompok Dalam Pekerjaan</b>
             <TableRow>
-              <TableCell colSpan={3}>{dataRPP.perankelompok}</TableCell>
+              <TableCell colSpan={3}>{dataRPP.group_role}</TableCell>
               <TableCell />
               <TableCell />
             </TableRow>
@@ -207,7 +228,7 @@ const DetailRPP = (props) => {
           <Table sx={{ minWidth: 650 }} aria-label="caption table">
             <b className="left">Deskripsi Pekerjaan</b>
             <TableRow>
-              <TableCell colSpan={3}>{dataRPP.deskripsi_tugas}</TableCell>
+              <TableCell colSpan={3}>{dataRPP.task_description}</TableCell>
               <TableCell />
               <TableCell />
             </TableRow>
@@ -228,7 +249,7 @@ const DetailRPP = (props) => {
                 <TableRow key={index}>
                   <TableCell>{index + 1}</TableCell>
                   <TableCell>{deliverable.deliverables}</TableCell>
-                  <TableCell>{deliverable.duedate}</TableCell>
+                  <TableCell>{deliverable.due_date}</TableCell>
                 </TableRow>
               )
             })}
@@ -249,9 +270,9 @@ const DetailRPP = (props) => {
               return (
                 <TableRow key={index}>
                   <TableCell>{index + 1}</TableCell>
-                  <TableCell>{milestone.tanggalmulai}</TableCell>
-                  <TableCell>{milestone.tanggalselesai}</TableCell>
-                  <TableCell>{milestone.deskripsi}</TableCell>
+                  <TableCell>{milestone.start_date}</TableCell>
+                  <TableCell>{milestone.finish_date}</TableCell>
+                  <TableCell>{milestone.description}</TableCell>
                 </TableRow>
               )
             })}
@@ -273,9 +294,9 @@ const DetailRPP = (props) => {
               return (
                 <TableRow key={index}>
                   <TableCell>{index + 1}</TableCell>
-                  <TableCell>{capaian.tanggalmulai}</TableCell>
-                  <TableCell>{capaian.tanggalberakhir}</TableCell>
-                  <TableCell colSpan={2}>{capaian.rencanacapaian}</TableCell>
+                  <TableCell>{capaian.start_date}</TableCell>
+                  <TableCell>{capaian.finish_date}</TableCell>
+                  <TableCell>{capaian.achievement_plan}</TableCell>
                   <TableCell/>
                 </TableRow>
               )
@@ -299,10 +320,10 @@ const DetailRPP = (props) => {
               return (
                 <TableRow key={index}>
                   <TableCell>{index + 1}</TableCell>
-                  <TableCell>{keseluruhan.tanggalmulai}</TableCell>
-                  <TableCell>{keseluruhan.tanggalselesai}</TableCell>
-                  <TableCell>{keseluruhan.jenispekerjaan}</TableCell>
-                  <TableCell>{keseluruhan.butirpekerjaan}</TableCell>
+                  <TableCell>{keseluruhan.start_date}</TableCell>
+                  <TableCell>{keseluruhan.finish_date}</TableCell>
+                  <TableCell>{keseluruhan.task_type}</TableCell>
+                  <TableCell>{keseluruhan.task_name}</TableCell>
                   <TableCell/>
                 </TableRow>
               )
@@ -310,7 +331,7 @@ const DetailRPP = (props) => {
           </Table>
         </TableContainer>
       </div>
-            */}
+           
                  <FloatButton type='primary' onClick={()=>history.push(`/rencanaPenyelesaianProyek`)} icon={<ArrowLeftOutlined />} tooltip={<div>Kembali ke Rekap RPP</div>} />
                  
         
